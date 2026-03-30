924a51fbf30c520b0ba2221f4dedac1ab6384173
Deepansh_5 <104337001+DeepanshK5@users.noreply.github.com>
2026-03-30 23:10:40 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String ImageName, String ImageTag){
   echo 'Image build started'
-  sh 'docker build -t ${ImageName}:${ImageTag} .'
+  sh "docker build -t ${ImageName}:${ImageTag} ."
   echo 'Image build successful'
 }
